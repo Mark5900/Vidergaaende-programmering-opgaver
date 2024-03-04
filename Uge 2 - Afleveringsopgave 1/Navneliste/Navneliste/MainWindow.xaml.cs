96002e49274821d4a619af93897fce829dab8957
Mark B. Rasmussen <mark@boennelykke.com>
2024-03-04 20:46:39 +0100

    Add GUI to navneliste

---
+++ Uge 2 - Afleveringsopgave 1/Navneliste/Navneliste/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Navneliste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRemovePosition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddName_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortAscending_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortDescending_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveSelectedName_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
