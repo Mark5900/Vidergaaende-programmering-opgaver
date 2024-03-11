34ce61b0bf685d46ff445b9914969ef1737612d9
Mark B. Rasmussen <mark@boennelykke.com>
2024-03-11 17:15:14 +0100

    Palindromer  opgave 1 og 2

---
+++ Palindromer/Palindromer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+while (true) { 
+    Console.WriteLine("Write a text, a number or a decimal number to check if it is a palindrome");
+    string input = Console.ReadLine();
+
+    bool isPalindrome = IsPalindrome(input);
+    Console.WriteLine(isPalindrome ? "It is a palindrome" : "It is not a palindrome");
+
+    Console.WriteLine();
+    Console.WriteLine("Do you want to check another one? (y/n)");
+    string answer = Console.ReadLine();
+    if (answer.ToLower() != "y")
+    {
+        break;
+    } else
+    {
+        Console.WriteLine();
+    }
+}
+
+static bool IsPalindrome(string s)
+{
+    s = RemoveSpecialCharacters(s);
+    s = s.ToLower();
+
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        if (s[i] != s[s.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static string RemoveSpecialCharacters(string str)
+{
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < str.Length; i++)
+    {
+        if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+        {
+            sb.Append(str[i]);
+        }
+    }
+    return sb.ToString();
+}
---
+++ Palindromer/README.md
@@ -0,0 +1,21 @@
+# Opgave: Palindromer
+
+## Opgave 1
+
+Lav en console applikation, der kan fortælle om det ord brugeren indtaster er et palindrom eller ej. Et palindrom er et ord, der staves ens både forfra og bagfra. Din applikation skal se bort fra store og små bogstaver, når der laves en sammenligning.
+
+## Opgave 2
+
+Ændre din applikation til at ignorere mellemrum, komma, bindestreger og andre special karaktere. Det vil sige at en bruger godt kan indtaste en sætning, som vil blive kontrolleret.
+
+## Opgave: Palindromer fortsat
+
+Som en fortsættelse af opgaven med palindromer i uge 1, skal du ændre din applikation sådan at din kontrol af den indtastede værdi foregår i en metode.
+
+Din applikation skal ud over at kunne kontrollere almindelige tekster også kunne kontrollere heltal og kommatal. Det vil sige, at du skal lave 3 metoder med hver deres signatur. Nedenfor kan du se nogle værdier du kan benytte til at teste med.
+
+- abba = palindrom
+- 12321 = palindrom
+- 123,21 = palindrom
+- Stack Cats = palindrom
+- A man, a plan, a canal – Panama = palindrom
